37db0accfd8c7596aa14bebbef062d7d47f128e9
Dillon Buchanan <thedillonb@gmail.com>
2013-05-14 19:32:50 -0400

    Added new images

--- MobilePatterns/Controllers/SettingsViewController.cs
+++ MobilePatterns/Controllers/SettingsViewController.cs
@@ -30,7 +30,7 @@ namespace MobilePatterns.Controllers
 			
 			var aboutSection = new Section("About", "Thank you for downloading. Enjoy!");
 			root.Add(aboutSection);
-			aboutSection.Add(new StyledElement("Follow On Twitter", () => UIApplication.SharedApplication.OpenUrl(new NSUrl("https://twitter.com/Codebucketapp"))));
+			aboutSection.Add(new StyledElement("Follow On Twitter", () => UIApplication.SharedApplication.OpenUrl(new NSUrl("https://twitter.com/appreciateuiapp"))));
 			aboutSection.Add(new StyledElement("Rate This App", () => UIApplication.SharedApplication.OpenUrl(new NSUrl("https://itunes.apple.com/us/app/codebucket/id551531422?mt=8"))));
 			aboutSection.Add(new StyledElement("App Version", NSBundle.MainBundle.InfoDictionary.ValueForKey(new NSString("CFBundleVersion")).ToString()));
 

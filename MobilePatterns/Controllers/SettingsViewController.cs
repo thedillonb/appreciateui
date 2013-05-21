fd7a68025b5b72945511bc2d49a94055df27abe2
Dillon Buchanan <thedillonb@gmail.com>
2013-05-21 01:38:17 -0400

    Change Settings controller

--- MobilePatterns/Controllers/SettingsViewController.cs
+++ MobilePatterns/Controllers/SettingsViewController.cs
@@ -22,7 +22,7 @@ namespace MobilePatterns.Controllers
 
 			var supportSection = new Section("Support");
 			root.Add (supportSection);
-			//supportSection.Add(new StyledElement("Technical Support", () => UIApplication.SharedApplication.OpenUrl(new NSUrl("https://twitter.com/Codebucketapp"))));
+			supportSection.Add(new StyledElement("Technical Support", () => UIApplication.SharedApplication.OpenUrl(new NSUrl("http://appreciateui.freshdesk.com/"))));
 
 			if (MFMailComposeViewController.CanSendMail)
 				supportSection.Add(new StyledElement("Contact Me", OpenMailer));
@@ -31,7 +31,7 @@ namespace MobilePatterns.Controllers
 			var aboutSection = new Section("About", "Thank you for downloading. Enjoy!");
 			root.Add(aboutSection);
 			aboutSection.Add(new StyledElement("Follow On Twitter", () => UIApplication.SharedApplication.OpenUrl(new NSUrl("https://twitter.com/appreciateuiapp"))));
-			//aboutSection.Add(new StyledElement("Rate This App", () => UIApplication.SharedApplication.OpenUrl(new NSUrl("https://itunes.apple.com/us/app/codebucket/id551531422?mt=8"))));
+			aboutSection.Add(new StyledElement("Rate This App", () => UIApplication.SharedApplication.OpenUrl(new NSUrl("https://itunes.apple.com/us/app/appreciateui/id651317060?ls=1&mt=8"))));
 			aboutSection.Add(new StyledElement("App Version", NSBundle.MainBundle.InfoDictionary.ValueForKey(new NSString("CFBundleVersion")).ToString()));
 
 			
